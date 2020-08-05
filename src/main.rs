5bb30ff9ae3242c8a5c41b9219dd5b55adf4f0a7
Daniel Trnka <daniel.trnka@gmail.com>
2020-08-05 23:42:52 +0100

    use nicer interface for commands

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ mod framebuffer;
 
 use video::{MappedVideo, VideoAnimation};
 use framebuffer::{FrameBuffer, Color};
-use ssd1351::Display;
+use ssd1351::*;
 
 fn main() {
     let mut chip = Chip::new("/dev/gpiochip0").unwrap();
@@ -37,62 +37,12 @@ fn main() {
     const HEIGHT: usize = 128;
 
     let mut fb = FrameBuffer::new();
-    let mut disp = Display::new(0, 0, dc);
-
-    disp.configa(0xFD, &[0x12u8]);               // Unlock IC MCU interface
-    disp.configa(0xFD, &[0xB1]);               // Command A2,B1,B3,BB,BE,C1 accessible if in unlock state
-    disp.configa(0xAE, &[]);                     // Display off
-    disp.configa(0xB3, &[0xF1]);               // Clock divider
-    disp.configa(0xCA, &[0x7F]);               // Mux ratio
-    disp.configa(0x15, &[0x00, WIDTH as u8 - 1]);    // Set column address
-    disp.configa(0x75, &[0x00, HEIGHT as u8 - 1]);   // Set row address
-    disp.configa(0xA0, &[0x70 | 0x00]);  // Segment remapping
-    disp.configa(0xA1, &[0x00]);               // Set Display start line
-    disp.configa(0xA2, &[0x00]);               // Set display offset
-    disp.configa(0xB5, &[0x00]);               // Set GPIO
-    disp.configa(0xAB, &[0x01]);               // Function select (internal - diode drop);
-    disp.configa(0xB1, &[0x32]);               // Precharge
-    disp.configa(0xB4, &[0xA0, 0xB5, 0x55]);   // Set segment low voltage
-    disp.configa(0xBE, &[0x05]);               // Set VcomH voltage
-    disp.configa(0xC7, &[0x0F]);               // Contrast master
-    disp.configa(0xB6, &[0x01]);               // Precharge2
-    disp.configa(0xA6, &[]);                     // Normal display
-    disp.configa(0xc1, &[0xff, 0xff, 0xff]);
-    disp.configa(0xAf, &[]);                     // Display off
-    disp.configa(0x15, &[0, 127]);                     // Write RAM
-    disp.configa(0x75, &[0, 127]);                     // Write RAM
-    disp.configa(0x5C, &[]);                     // Write RAM
-
-    /*
-    disp.config(CommandSetCommandLock(MCUProtection::Unlock));
-    disp.config(CommandSetCommandLock(MCUProtection::AdditionalCommandsInUnlock));
-    disp.config(CommandSetSleepModeOn); 
-    disp.config(CommandSetSleepModeOff); 
-    disp.config(CommandSetColumnAddress(0, width - 1));
-    disp.config(CommandSetRowAddress(0, height - 1));
-    disp.config(CommandWriteRam);
-    */
-
-    /*
-    disp.config(CommandFrontClockDivOscilatorFreq {
-        frontClkDiv: FrontClockDiv::Div1024,
-        oscilatorFreq: 
-    });
-    */
-
-    fb.set(0, 0, Color::new(255, 0, 0));
-    for x in 30..50 {
-        for y in 30..50 {
-            fb.set(x, y, Color::new(255, 0, 0));
-        }
-    }
-
-
-//    disp.display(&fb);
-
+    let mut disp = Display::new(0, 0, dc).unwrap();
+    
+	
 	let video = MappedVideo::new("../disp/vsb.raw", WIDTH, HEIGHT, 2).unwrap();
 	let anim = VideoAnimation::new(&video);
 	for frame in anim {
-		disp.displaya(frame);
+		disp.render(frame);
 	}
 }
--- src/ssd1351.rs
+++ src/ssd1351.rs
@@ -6,18 +6,48 @@ use byteorder::{LittleEndian, WriteBytesExt};
 pub struct Display {
    spi: Spidev,
    dc: LineHandle,
+   width: usize,
+   height: usize,
 }
 
-enum Command {
+pub enum Interface {
+    Parallel8 = 0b00,
+    Parallel16 = 0b01,
+    Parallel18 = 0b11,
+}
+
+pub enum PinState {
+    HiZInputDisabled,
+    HiZInputEnabled,
+    OutputLow,
+    OutputHigh,
+}
+
+pub enum ScrollingInterval {
+    TestMode,
+    Normal,
+    Slow,
+    Slowest,
+}
+
+pub enum ColorDepth {
+    Color65k = 0b00,
+    Color262k = 0b10,
+    Color262kFmt2 = 0b11,
+}
+
+pub enum Command {
    SetColumnAddress(u8, u8),
    SetRowAddress(u8, u8),
    WriteRam,
    ReadRam,
    SetReMapColorDepth{
-    verticalAddressIncrement: bool,
-    columnAddress127ToSeg0: bool,
-    colorSwapped: bool,
-    // TODO: xd
+    vert_increment: bool,
+    column_invert: bool,
+    swap_colors: bool,
+    scan_from_n: bool,
+    enable_com_split: bool,
+    color_depth: ColorDepth,
    },
    SetDisplayStartLine(u8),
    SetDisplayOffset(u8),
@@ -26,27 +56,54 @@ enum Command {
    SetDisplayModeNormal,
    SetDisplayModeInverse,
    FunctionSelection{
-    intervalRegulator: bool,
-    //mode
+    internalRegulator: bool,
+    interface: Interface,
    },
    Nop,
    SetSleepModeOn,
    SetSleepModeOff,
-   Nop2,
-   SetResetPreCharge, //
-   DisplayEnhancement, //
-
-   FrontClockDivOscilatorFreq {
-    frontClkDiv: FrontClockDiv,
-    oscilatorFreq: u8,
+   SetResetPreCharge {
+    phase_1: u8,
+    phase_2: u8,
    },
-
-
-   SleepOn,
-   SetCommandLock(MCUProtection)
+   DisplayEnhancement(bool),
+   Clocks {
+    divider: ClkDiv,
+    osc_freq: u8,
+   },
+   SetSegmentLowVoltage{
+       external: bool
+   },
+   SetGPIO {
+    gpio0: PinState,
+    gpio1: PinState,
+   },
+   SetSecondPreChargePeriod {
+    precharge_period: u8,
+    gs_table: [u8; 63],
+   },
+   UseBuiltInLUT,
+   SetPreChargeVoltage(u8),
+   SetVComVoltage(u8),
+   SetContrast{
+    r: u8,
+    g: u8,
+    b: u8,
+   },
+   MasterContrast(u8),
+   SetMuxRatio(u8),
+   SetCommandLock(MCUProtection),
+   HorizontalScroll {
+    scroll: u8,
+    start_row: u8,
+    rows: u8,
+    interval: ScrollingInterval,
+   },
+   StopMoving,
+   StartMoving,
 }
 
-enum FrontClockDiv {
+pub enum ClkDiv {
     Div1,
     Div2,
     Div4,
@@ -60,31 +117,116 @@ enum FrontClockDiv {
     Div1024,
 }
 
-enum MCUProtection {
+pub enum MCUProtection {
     Unlock = 0x12,
     LockCommands = 0x16,
     AdditionalCommandsInaccessible = 0xb0,
-    AdditionalCommandsInUnlock = 0x1b,
+    AdditionalCommandsInUnlock = 0xb1,
+}
+
+pub enum TransferError {
+    SPIError(std::io::Error),
+    GPIOError(gpio_cdev::errors::Error),
+    PartialTransfer(usize),
+}
+
+impl From<std::io::Error> for TransferError {
+	fn from(error: std::io::Error) -> Self {
+		TransferError::SPIError(error)
+	}
+}
+
+impl From<gpio_cdev::errors::Error> for TransferError {
+	fn from(error: gpio_cdev::errors::Error) -> Self {
+		TransferError::GPIOError(error)
+	}
 }
 
 impl Display {
-    pub fn new(spidev: u32, cs: u32, dc_line: LineHandle) -> Self {
-        let mut spi = Spidev::open("/dev/spidev0.0").unwrap();
+    pub fn new(spidev: u32, cs: u32, dc_line: LineHandle) -> Result<Self, std::io::Error> {
+        let mut spi = Spidev::open(format!("/dev/spidev{}.{}", spidev, cs))?;
 		let options = SpidevOptions::new()
 			 .bits_per_word(8)
 			 .max_speed_hz(5_000_000)
 			 .mode(SpiModeFlags::SPI_MODE_0)
 			 .build();
-		spi.configure(&options).unwrap();
-        Display {
+		spi.configure(&options)?;
+        let mut disp = Display {
             spi,
-            dc: dc_line
-        }
+            dc: dc_line,
+            width: 128,
+            height: 128,
+        };
+        disp.init();
+        Ok(disp)
     }
 
-    pub fn displaya(&mut self, fb: &[u8]) {
-        self.dc.set_value(1).unwrap();
-        self.spi.write(&fb);
+    fn init(&mut self) {
+        self.config(Command::SetCommandLock(MCUProtection::Unlock));
+        self.config(Command::SetCommandLock(MCUProtection::AdditionalCommandsInUnlock));
+        self.config(Command::SetSleepModeOn); 
+        self.config(Command::Clocks {
+            divider: ClkDiv::Div2,
+            osc_freq: 0xF,
+        });
+        self.config(Command::SetMuxRatio(0x7f));
+        self.config(Command::SetColumnAddress(0, self.width as u8 - 1));
+        self.config(Command::SetRowAddress(0, self.height as u8 - 1));
+        self.config(Command::SetReMapColorDepth {
+            vert_increment: false,
+            column_invert: false,
+            swap_colors: false,
+            scan_from_n: true,
+            enable_com_split: true,
+            color_depth: ColorDepth::Color65k,
+        });
+        self.config(Command::SetDisplayStartLine(0));
+        self.config(Command::SetDisplayOffset(0));
+        self.config(Command::SetGPIO {
+            gpio0: PinState::HiZInputDisabled, 
+            gpio1: PinState::HiZInputDisabled, 
+        });
+        self.config(Command::FunctionSelection{
+            internalRegulator: true,
+            interface: Interface::Parallel8, 	
+        });
+
+        //
+        self.config(Command::SetResetPreCharge{
+            phase_1: 0x2,
+            phase_2: 0x3
+        });
+        self.config(Command::SetSegmentLowVoltage{
+            external: true
+        });
+        self.config(Command::SetVComVoltage(0x05));
+        self.config(Command::MasterContrast(0x0F));
+        self.config(Command::SetPreChargeVoltage(0x01));
+        self.config(Command::SetDisplayModeNormal);
+        self.config(Command::SetContrast{
+            r: 0xff,
+            g: 0xff,
+            b: 0xff,
+        });
+        self.config(Command::SetSleepModeOff);
+        self.config(Command::WriteRam);
+    }
+
+    pub fn render(&mut self, fb: &[u8]) -> Result<(), TransferError> {
+        self.data_mode(true);
+        self.xmit(fb)
+    }
+
+    fn xmit(&mut self, data: &[u8]) -> Result<(), TransferError> {
+        let bufsize = data.len();
+        match self.spi.write(&data)? {
+            bufsize => Ok(()),
+            sent => Err(TransferError::PartialTransfer(sent))
+        }        
+    }
+
+    fn data_mode(&mut self, data: bool) -> Result<(), TransferError> {
+        Ok(self.dc.set_value(data as u8)?)
     }
 
     pub fn configa(&mut self, cmd: u8, data: &[u8]) {
@@ -98,41 +240,84 @@ impl Display {
 
     }
 
-    pub fn config(&mut self, option: Command) {
+    pub fn config(&mut self, option: Command) -> Result<(), TransferError> {
         let (cmd, data) = match option {
             Command::SetColumnAddress(start, end) => (0x15, vec![start, end]),
             Command::SetRowAddress(start, end) => (0x75, vec![start, end]),
             Command::WriteRam => (0x5c, Vec::new()),
             Command::ReadRam => (0x5d, Vec::new()),
-
-
-        
-
-
-            Command::SleepOn => (0xFF, Vec::new()),
-
             Command::SetSleepModeOn => (0xae, Vec::new()),
             Command::SetSleepModeOff => (0xaf, Vec::new()),
             Command::SetCommandLock(prot) => (0xfd, vec![prot as u8]),
-
-            Command::FrontClockDivOscilatorFreq{frontClkDiv, oscilatorFreq} => 
-                match oscilatorFreq {
+            Command::Clocks {divider, osc_freq} => 
+                match osc_freq {
                     0..=0b1111 => (0xb3, vec![
-                                   frontClkDiv as u8 | ((oscilatorFreq as u8) << 4)
+                                   divider as u8 | ((osc_freq as u8) << 4)
                     ]),
                     _ => unreachable!(),
-                }
-            ,
+                },
+            Command::SetMuxRatio(ratio) => match ratio {
+                15..=127 => (0xca, vec![ratio]), 
+                _ => unreachable!(),
+            },
+           Command::SetReMapColorDepth{
+            vert_increment,
+            column_invert,
+            swap_colors,
+            scan_from_n,
+            enable_com_split,
+            color_depth
+           } => (0xa0, vec![
+                 (vert_increment as u8) |
+                 ((column_invert as u8) << 1) |
+                 ((swap_colors as u8) << 2) |
+                 ((scan_from_n as u8) << 4) |
+                 ((enable_com_split as u8) << 5) |
+                 ((color_depth as u8) << 6)
+            ]),
+            Command::SetDisplayStartLine(line) => match line {
+                0..=127 => (0xA1, vec![line]),
+                _ => unreachable!(),
+            },
+            Command::SetDisplayOffset(line) => match line {
+                0..=127 => (0xA2, vec![line]),
+                _ => unreachable!(),
+            },
+            Command::SetGPIO{gpio0, gpio1} => (0xb5, vec![(gpio0 as u8) | ((gpio1 as u8) >> 2)]),
+            Command::FunctionSelection{internalRegulator, interface} => (0xab, vec![(internalRegulator as u8) | ((interface as u8) << 6)]),
+            Command::SetResetPreCharge{phase_1, phase_2} => match (phase_1, phase_2) {
+                (2..=15, 3..=15) => (0xb1, vec![phase_1 | (phase_2 << 4)]),
+                _ => unreachable!(),
+            },
+            Command::SetSegmentLowVoltage{external} => (0xb4, vec![0xa0 | external as u8, 0b10110101, 0b01010101]),
+            Command::SetVComVoltage(val) => match val {
+                0..=0b111 => (0xbe, vec![val]),
+                _ => unreachable!(),
+            },
+            Command::MasterContrast(val) => match val {
+                0..=0b1111 => (0xc7, vec![val]), 
+                _ => unreachable!(),
+            },
+            Command::SetPreChargeVoltage(val) => match val {
+                0..=0b11111 => (0xbb, vec![val]),
+                _ => unreachable!(),
+            },
+            Command::SetContrast{r, g, b} => (0xc1, vec![r, g, b]),
+            Command::SetDisplayModeOff => (0xa4, Vec::new()),
+            Command::SetDisplayModeOn => (0xa5, Vec::new()),
+            Command::SetDisplayModeNormal => (0xa6, Vec::new()),
+            Command::SetDisplayModeInverse => (0xa7, Vec::new()),
 
-            _ => panic!("xd"),
+            _ => unreachable!()
         };        
 
-        self.dc.set_value(0).unwrap();
-        self.spi.write(&[cmd]);
+        self.data_mode(false);
+        self.xmit(&[cmd])?;
 
         if !data.is_empty() {
-            self.dc.set_value(1).unwrap();
-            self.spi.write(&data);
+            self.data_mode(true);
+            self.xmit(&data)?;
         }
+        Ok(())
     }
 }
