704f680b59308c003ce93c5f067e9fba6e499193
Daniel Trnka <daniel.trnka@gmail.com>
2020-08-06 09:23:58 +0100

    Keep constant framerate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![allow(dead_code)]
 
 extern crate spidev;
+use std::{thread, time};
+use std::time::{Instant, Duration};
 use gpio_cdev::{Chip, LineRequestFlags};
 use ssd1351::*;
-use std::{thread, time};
 use video::{MappedVideo, VideoAnimation};
 
 mod framebuffer;
@@ -15,6 +16,7 @@ fn main() -> Result<(), Box<dyn ::std::error::Error>> {
     const GPIO_DC: u32 = 24;
     const WIDTH: usize = 128;
     const HEIGHT: usize = 128;
+    const FPS: u32 = 25;
 
     let mut chip = Chip::new("/dev/gpiochip0")?;
 
@@ -32,8 +34,11 @@ fn main() -> Result<(), Box<dyn ::std::error::Error>> {
 
     let video = MappedVideo::new("../disp/vsb.raw", WIDTH, HEIGHT, 2)?;
     let anim = VideoAnimation::new(&video);
+    let frames_period = Duration::from_secs_f32(1.0 / (FPS as f32));
     for frame in anim {
+        let start = Instant::now();
         disp.render(frame)?;
+        thread::sleep(frames_period - start.elapsed());
     }
     Ok(())
 }
